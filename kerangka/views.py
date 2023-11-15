30abbd0f2a6ffc56ee0873dc826f66cbad17de4e
kholqi177 <mohkholqimaulana@gmail.com>
2023-11-15 15:17:08 +0700

    udah

--- kerangka/views.py
+++ kerangka/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,HttpResponse
-
+from.models import*
 # Create your views here.
 def mulai(request):
-    return render(request, 'home.html')
+    biodata = Biodata.objects.all()
+    return render(request, 'home.html',{"data":biodata})
 
 def home(request):
     return render(request,'halaman2.html')
